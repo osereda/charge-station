61da1931735ae930a329a1560b4eae4795f2dd52
osereda <osereda.dev@gmail.com>
2021-02-12 17:49:50 +0200

    added new logic

--- routes/slot.hardware.routes.js
+++ routes/slot.hardware.routes.js
@@ -23,37 +23,21 @@ router.get ("/:id", (req, res) => {
     let stationStatus = null;
     let newSlotCollection = [];
     let countOfSlot = 0;
-    let tmp1 = '';
-    let tmp2 = '';
-    let tmp3 = 0;
 
     if(req.params.id) {
-        logger.info("paramStr: "+paramStr);
         const param = paramStr.replace(/\s/g, '');
-        logger.info("param1: "+param);
         let count = (param.match(/&/g) || []).length;
         for(let i = 0; i < count; countOfSlot++) {
             slotId = parseInt(param.split('&')[i++].split('=')[1]);
-            // scooterId = parseInt(param.split('&')[i++].split('=')[1]);
-
-            logger.info("param2: "+param);
-            tmp1 = param.split('&')[i++];
-            logger.info("tmp1: "+tmp1);
-            tmp2 = tmp1.split('=')[1];
-            logger.info("tmp2: "+tmp2);
-            tmp3 = parseInt(tmp2,16);
-            logger.info("tmp3: "+tmp3);
-
+            scooterId = parseInt(param.split('&')[i++].split('=')[1]);
             status = param.split('&')[i++].split('=')[1];
             slotPower = param.split('&')[i++].split('=')[1];
-            console.log("sl_id:  " + slotId);
-            // console.log("sc_id:  " + scooterId);
-            logger.info("slot update param - sl_id:" + slotId
-                + " sc_id:  " + scooterId + " status: " + status + "slot power: " + slotPower);
+            // logger.info("slot update param - sl_id:" + slotId
+            //     + " sc_id:  " + scooterId + " status: " + status + "slot power: " + slotPower);
             let tmp = {
                 slot_id: slotId,
                 newDate: {
-                    scooter_id: tmp3,
+                    scooter_id: scooterId,
                     slot_status: status,
                     slot_power: slotPower
                 }
@@ -62,9 +46,9 @@ router.get ("/:id", (req, res) => {
         }
         try {
             stationStatus = null;
-            Scooter.find({sc_id: tmp3})
+            Scooter.find({sc_id: scooterId})
                 .then ( scooter => {
-                    if(tmp3 !== 0 && scooter.length === 0){
+                    if(scooterId !== 0 && scooter.length === 0){
                         stationStatus=0;
                         console.log("slot update - scooter not found: " + scooter.length);
                         logger.info("slot update - scooter not found: " + scooterId);
@@ -84,8 +68,6 @@ router.get ("/:id", (req, res) => {
                                     res.send({status: stationStatus});
                                 }
                                 else {
-                                    logger.info("slot update - OK: " + item.slot_id);
-                                    console.log("slot update - OK");
                                     res.send({status: 1});
                                 }
                             });
