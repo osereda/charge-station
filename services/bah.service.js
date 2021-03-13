955962e25057ee29d65492b1e4c4160c76bc2e5a
osereda <osereda.dev@gmail.com>
2021-03-13 13:47:42 +0200

    added new logic

--- services/bah.service.js
+++ services/bah.service.js
@@ -11,9 +11,9 @@ class BahService {
     async addBah(inputData) {
         if(inputData) {
             const latRecordBah = await Bah.getLastRecord();
-            const checkDate = true;//latRecordBah[0]._doc.bah_date.getDate() === new Date().getDate() ? false : true;
+            const checkDate = latRecordBah[0]._doc.bah_date.getDate() === new Date().getDate() ? false : true;
             if(checkDate) {
-                const currentAld = 100;//latRecordBah[0]._doc.bah_balance_current;
+                const currentAld = latRecordBah[0]._doc.bah_balance_current;
                 const amount = inputData * 0.01;
                 const currentNew = currentAld - amount;
                 let newData = {
